4289d2f9f12e690d28728d147f02ebe299723938
valentina parada <valentina parada@LAPTOP-PPFIBUBV>
2022-06-09 14:40:52 -0500

    updateLightday

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cv_ejercicio;
 
 /**
- *
+
  * @author valentina parada
  */
 public class Cv_ejercicio {
@@ -16,6 +12,7 @@ public class Cv_ejercicio {
      */
     public static void main(String[] args) {
         System.out.println("Version 0");
+        System.out.println("version 1 ");
     }
     
 }
