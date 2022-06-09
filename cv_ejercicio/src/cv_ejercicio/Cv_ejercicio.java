af951f4bdf78ee75f2c1323f217140351c1ca16d
valentina parada <valentina parada@LAPTOP-PPFIBUBV>
2022-06-09 14:56:21 -0500

    updateLightday

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -13,7 +13,7 @@ public class Cv_ejercicio {
     public static void main(String[] args) {
         System.out.println("Version 0");
         System.out.println("version 1 ");
-        System.out.println("version 2");
+        System.out.println("version 2 para braunch valentina ");
     }
     
 }
